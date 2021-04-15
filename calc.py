d1e95174bbda76b451e7d41825093b33b884adbb
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-04-15 16:47:32 -0700

    diff step

--- calc.py
+++ calc.py
@@ -1,4 +1,11 @@
 def calc(a,b):
     sum = a + b
     print(sum)
+    diff = a - b
     
+# Write the code for subtract
+# Do a commit
+# Write the code for multiply
+# Do a commit
+# Write code for division
+# Do a commit
