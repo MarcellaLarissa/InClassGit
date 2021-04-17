9ba006ab05abe7c5ff10098895e7d8529102736f
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-04-16 21:19:04 -0700

    random working

--- password.py
+++ password.py
@@ -1,10 +1,13 @@
 import random
 
 def password(n):
-    list_A = [1,h,r,y,f,x,j,i,2, 8,g,r,j,8,g,w,c,9,h,v,0,b,j,o,2,a,x,z,3, 4, 8,5, 6]
-    random.randrange(20, 50)
-
+    passwordList = []
+    list_A = [1,"h","r","y","f","x","j","i",2,8,"g","r","j",8,"g","w","c",9,"h","v",0,"b","j","o",2,"a","x","z",3, 4, 8,5, 6]
+    while n > 0:
+        passwordList.append(random.choice(list_A[random.randrange(0, 30)]))
+        n = n - 1
+    print(passwordList)
 n = int(input("Please enter a number"))
 
-randinit()
+
 password(n)
