162215d6af9501b055bf21334efb0319e6e7173e
MarcellaLarissa <56004783+MarcellaLarissa@users.noreply.github.com>
2021-04-16 21:05:54 -0700

    added list, randrange call

--- password.py
+++ password.py
@@ -1,8 +1,10 @@
 import random
 
 def password(n):
-    pass
+    list_A = [1,h,r,y,f,x,j,i,2, 8,g,r,j,8,g,w,c,9,h,v,0,b,j,o,2,a,x,z,3, 4, 8,5, 6]
+    random.randrange(20, 50)
 
 n = int(input("Please enter a number"))
 
+randinit()
 password(n)
